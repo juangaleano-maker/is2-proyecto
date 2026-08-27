4d63b8eeb7367ca9185150654795b9f168993fa9
Carlos Barreto <carlosbc0985cb@gmail.com>
2026-08-27 20:24:22 -0300

    Cambios en Is2-28

---
+++ agregar_usuario/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AgregarUsuarioConfig(AppConfig):
+    name = 'agregar_usuario'
--- global_exchange/settings.py
+++ global_exchange/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clientes',
     'usuarios',
+    'agregar_usuario',
 ]
 
 MIDDLEWARE = [
