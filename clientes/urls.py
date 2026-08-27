e9c6f9c93e442bab0dd8b3c23d37faface6dc92e
Carlos Barreto <carlosbc0985cb@gmail.com>
2026-08-27 18:53:12 -0300

    SCRUM-IS2-12: Ajustes de selección de clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.elegir_cliente, name='elegir_cliente'),
+    path('seleccionarCliente/', views.elegir_cliente, name='elegir_cliente'),
 ]
+
