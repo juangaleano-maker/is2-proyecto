c50e03010c3850878b42625a436c7abb15093ac9
maramoons <maraparra48@gmail.com>
2026-08-31 11:51:16 -0300

    Fix:configuracion para el envio de correos

--- global_exchange/settings.py
+++ global_exchange/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = 'global_exchange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'global_exchange',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
